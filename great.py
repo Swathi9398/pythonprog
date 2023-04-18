76cb5ef6fe97cdd034fee263b9554dc792cd2dc5
Javid-Shaik <101865673+Javid-Shaik@users.noreply.github.com>
2023-04-18 13:24:52 +0530

    Latest.py

---
+++ great.py
@@ -0,0 +1,19 @@
+a , b , c = map(int,input("Enter three numbers seperated with space : ").split())
+
+if a==b:
+  print("a is equal to b")
+elif a==c:
+  print("a is equal to c")
+elif b==c:
+  print("b is equal to c")
+else:
+  print("No two numbers are equal")
+
+if a>b and a>c :
+  print("a = %d is big" %a)
+elif b>a and b>c :
+  print("b = %d is big" %b)
+else :
+  print("c = %d is big" %c)
+  
+

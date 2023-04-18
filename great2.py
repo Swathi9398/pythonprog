5c7b3ff2ed977dcdd388678e79212b5cce190d93
Javid-Shaik <101865673+Javid-Shaik@users.noreply.github.com>
2023-04-18 13:14:11 +0530

    Update great2.py

--- great2.py
+++ great2.py
@@ -1,4 +1,12 @@
 a , b , c = map(int,input("Enter three numbers seperated with space : ").split())
+
+if a==b:
+  print("a is equal to b")
+elif a==c:
+  print("a is equal to c")
+elif b==c:
+  print("b is equal to c")
+
 if a>b and a>c :
   print("a = %d is big" %a)
 elif b>a and b>c :
@@ -6,9 +14,4 @@ elif b>a and b>c :
 else :
   print("c = %d is big" %c)
   
-if a==b:
-  print("a is equal to b")
-elif a==c:
-  print("a is equal to c")
-elif b==c:
-  print("b is equal to c")
+
